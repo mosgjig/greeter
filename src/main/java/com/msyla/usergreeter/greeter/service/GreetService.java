ce5e98ffd777f61702ed78aec6c91891cb659770
Modest <modest.syla@gmail.com>
2016-02-11 20:54:19 +0100

    greeter-002 - Dock me up!

--- src/main/java/com/msyla/usergreeter/greeter/service/GreetService.java
+++ src/main/java/com/msyla/usergreeter/greeter/service/GreetService.java
@@ -2,6 +2,7 @@ package com.msyla.usergreeter.greeter.service;
 
 import com.msyla.usergreeter.greeter.dto.UserPreferenceDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,11 @@ public class GreetService {
 
     @Autowired
     private OAuth2RestTemplate restTemplate;
+    @Value("${user.host.ip}")
+    private String userHostIp;
 
     public String getGreetByUsername(String username) {
-        ResponseEntity<UserPreferenceDto[]> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/user/preferences?firstName=" + username, UserPreferenceDto[].class);
+        ResponseEntity<UserPreferenceDto[]> responseEntity = restTemplate.getForEntity("http://" + userHostIp + ":8080/api/user/preferences?firstName=" + username, UserPreferenceDto[].class);
         String s = "Hi " + responseEntity.getBody()[0].getFirstName() + ". Your Favourite season is " + responseEntity.getBody()[0].getSeason() + ".";
         return s;
     }
